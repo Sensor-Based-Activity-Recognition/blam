69c7670d0671106b2b32a534fe25840f06fcd164
Gabriel Torres Gamez <gabriel.torres@fuet.ch>
2023-03-04 12:53:51 +0100

    add zip support for reading data

--- wrangler.py
+++ wrangler.py
@@ -1,5 +1,7 @@
 import json
 import polars as pl
+
+from zipfile import ZipFile
 from questdb.ingress import Sender
 
 # read config.json file
@@ -35,7 +37,105 @@ class File:
         Returns the data as a polars DataFrame
 
         Returns:
-            data (polars.DataFrame): Dataframe with the data
+            polars.DataFrame: Dataframe with the data
+        """
+        # if path is a zip file, use get_data_zip
+        if self.path.lower().endswith(".zip"):
+            return self.get_data_zip()
+        # if path is a json file, use get_data_json
+        elif self.path.lower().endswith(".json"):
+            return self.get_data_json()
+        # else return None
+        else:
+            print("File type not supported")
+            return None
+
+    def get_data_zip(self):
+        """
+        Returns the zip data as a polars DataFrame
+
+        Returns:
+            polars.DataFrame: Dataframe with the data
+        """
+        # check if data is already loaded
+        if self.data is not None:
+            return
+        # initialize data
+        data = None
+        # iterate over files in zip
+        for i in ZipFile(self.path).namelist():
+            # only read csv files that are in sensors list
+            if (
+                i.split(".")[0] in (sensor.split("_")[0] for sensor in self.sensors)
+                and i.split(".")[-1] == "csv"
+            ):
+                # read csv
+                temp = pl.read_csv(ZipFile(self.path).read(i), sep=",").lazy()
+                # get sensor name
+                sensor = i.split(".")[0]
+                # melt data on time
+                temp = temp.melt(id_vars="time", value_name="value")
+                # change variable to include sensor name
+                temp = temp.with_columns(
+                    (pl.lit(f"{sensor}_") + pl.col("variable")).alias("variable")
+                )
+                # concat data if data is not None, else set data to temp
+                data = temp if data is None else pl.concat([data, temp])
+
+        # change resolution of data to 10ms
+        data = data.with_columns(
+            (pl.col("time") // 10000000 * 10).cast(pl.Int64).alias("time")
+        )
+
+        # change time measurements from nanoseconds to milliseconds
+        data = data.with_columns(pl.from_epoch("time", unit="ms").alias("time"))
+
+        # convert values to float if possible
+        data = data.with_columns(
+            pl.col("value").cast(pl.Float32, strict=False).alias("value")
+        )
+
+        # filter sensors
+        data = data.filter(pl.col("variable").is_in(self.sensors))
+
+        # collect 1
+        data = data.collect()
+
+        # pivot the data + lazy
+        data = data.pivot(
+            index="time",
+            columns="variable",
+            values="value",
+            aggregate_function="mean",
+            sort_columns=True,
+        ).lazy()
+
+        # add file name to columns as filename
+        data = data.with_columns(
+            pl.lit(self.path.split("/")[-1].split(".")[0]).alias("filename")
+        )
+
+        # add person name to columns as person
+        data = data.with_columns(pl.lit(self.path.split("/")[-2]).alias("person"))
+
+        # add activity name to columns as activity
+        data = data.with_columns(pl.lit(self.path.split("/")[-3]).alias("activity"))
+
+        # collect 2
+        data = data.collect()
+
+        # set data to self.data
+        self.data = data
+
+        # return data
+        return self.data
+
+    def get_data_json(self):
+        """
+        Returns the json data as a polars DataFrame
+
+        Returns:
+            polars.DataFrame: Dataframe with the data
         """
         # check if data is already loaded
         if self.data is None:
