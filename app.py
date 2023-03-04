8d71a44872ec342bad50fc18ee86e5ca0eec2fbb
Gabriel Torres Gamez <gabriel.torres@fuet.ch>
2023-03-04 17:58:55 +0100

    first version of working app

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import json
 
 # tkinter imports
 from tkinter import *
+from tkinter import messagebox
 from tkinter import filedialog
 
 # utils code imports
@@ -17,28 +18,52 @@ with open("config.json", "r") as f:
 sensors = config["sensors"]
 OneDriveFolder = config["OneDriveFolder"]
 questdb_settings = config["questdb"]
+dev = config["dev"]
+
+# set up tkinter and hide root window
+root = Tk()
+root.withdraw()
 
 # give file selector dialog
-root = Tk()
 root.filename = filedialog.askopenfilename(
     initialdir=OneDriveFolder,
     title="Select file",
     filetypes=(("zip file", "*.zip"), ("json file", "*.json")),
 )
+# if no file is selected, exit program
+if not root.filename:
+    # show error message with tk
+    messagebox.showerror("Error", "No file selected")
+    # exit program
+    exit(1)
 
 # get file
 file = File(path=root.filename, sensors=sensors)
 data = file.get_data().to_pandas().set_index("time")
 
 # get data selector
-selector = DataSelector(
-    df=data, title_prefix=root.filename.split("/")[-1], show_cols=sensors[:3]
-)
+try:
+    truncated_data = (
+        DataSelector(
+            df=data, title_prefix=root.filename.split("/")[-1], show_cols=sensors[:3]
+        )
+        .truncate()
+        .reset_index(drop=False)
+    )
+except Exception as e:
+    # show error message with tk
+    messagebox.showerror("Error", e)
+    # exit program
+    exit(1)
 
-# get truncated data
-truncated_data = selector.truncate()
+# prompt if write to database
+if messagebox.askyesno("Write to database", "Write to database?"):
+    # get database name
+    db_name = "dev" if dev else "prod"
+    # write to database
+    if status := file.write_data(questdb_settings, truncated_data, db_name):
+        messagebox.showinfo("Success", "Write successful")
+    else:
+        messagebox.showerror("Error", "Write unsuccessful")
 
-# write truncated data to database
-# status = file.write_data(questdb_settings, truncated_data, "dev")
-# print status of import
-# print("Write successful: ", status)
+exit()
