f1a491ca791b0345f91f4b4e6401e640aa857d1c
buesst1 <tobias.buess2001@gmail.com>
2023-03-17 19:31:55 +0100

    select multiple files

--- app.py
+++ app.py
@@ -29,58 +29,60 @@ root = Tk()
 root.withdraw()
 
 # give file selector dialog
-root.filename = filedialog.askopenfilename(
+filenames = filedialog.askopenfilenames(
     initialdir=OneDriveFolder,
     title="Select file",
     filetypes=(("all", "*.*"), ("zip file", "*.zip"), ("json file", "*.json")),
 )
-# if no file is selected, exit program
-if not root.filename:
-    # show error message with tk
-    messagebox.showerror("Error", "No file selected")
-    # exit program
-    exit(1)
 
-# check if file has already been processed
-person = root.filename.split("/")[-2]
-activity = root.filename.split("/")[-3]
-short_filename = root.filename.split("/")[-1].split(".")[0]
+for filename in filenames:
+    # if no file is selected, exit program
+    if not filename:
+        # show error message with tk
+        messagebox.showerror("Error", "No file selected")
+        # exit program
+        exit(1)
 
-if n_datavals := Database().get_scalar(
-    f"SELECT count(*) AS value FROM {db_name} WHERE filename %3D '{short_filename}' and person %3D '{person}' and activity %3D '{activity}'"
-):
-    msg_box = messagebox.askyesno(
-        "File already processed",
-        f"File already processed with {n_datavals} values in Database. Continue?",
-    )
-    if not msg_box:
-        exit(0)
+    # check if file has already been processed
+    person = filename.split("/")[-2]
+    activity = filename.split("/")[-3]
+    short_filename = filename.split("/")[-1].split(".")[0]
 
-# get file
-file = File(path=root.filename, sensors=sensors)
-data = file.get_data().to_pandas().set_index("time")
-
-# get data selector
-try:
-    truncated_data = (
-        Selector(
-            df=data, title_prefix=root.filename.split("/")[-1], show_cols=sensors[:3]
+    if n_datavals := Database().get_scalar(
+        f"SELECT count(*) AS value FROM {db_name} WHERE filename %3D '{short_filename}' and person %3D '{person}' and activity %3D '{activity}'"
+    ):
+        msg_box = messagebox.askyesno(
+            "File already processed",
+            f"File already processed with {n_datavals} values in Database. Continue?",
         )
-        .truncate()
-        .reset_index(drop=False)
-    )
-except Exception as e:
-    # show error message with tk
-    messagebox.showerror("Error", e)
-    # exit program
-    exit(1)
+        if not msg_box:
+            continue #skip 
 
-# prompt if write to database
-if messagebox.askyesno("Write to database", "Write to database?"):
-    # write to database
-    if status := file.write_data(questdb_settings, truncated_data, db_name):
-        messagebox.showinfo("Success", "Write successful")
-    else:
-        messagebox.showerror("Error", "Write unsuccessful")
+    # get file
+    file = File(path=filename, sensors=sensors)
+    data = file.get_data().to_pandas().set_index("time")
+
+    # get data selector
+    try:
+        truncated_data = (
+            Selector(
+                df=data, title_prefix=filename.split("/")[-1], show_cols=sensors[:3]
+            )
+            .truncate()
+            .reset_index(drop=False)
+        )
+    except Exception as e:
+        # show error message with tk
+        messagebox.showerror("Error", e)
+        # exit program
+        exit(1)
+
+    # prompt if write to database
+    if messagebox.askyesno("Write to database", "Write to database?"):
+        # write to database
+        if status := file.write_data(questdb_settings, truncated_data, db_name):
+            messagebox.showinfo("Success", "Write successful")
+        else:
+            messagebox.showerror("Error", "Write unsuccessful")
 
 exit()
