822e9fec61b9406bb1fe06bf0554c19a948b234b
buesst1 <tobias.buess2001@gmail.com>
2023-06-04 15:49:16 +0200

    added docstrings and comments

--- utils/dbconnector.py
+++ utils/dbconnector.py
@@ -4,31 +4,48 @@ import requests
 
 import polars as pl
 
-# read config
+# Load database configuration
 with open("config.json") as f:
     config = json.load(f)
-questdb_settings = config["questdb"]
-
+questdb_settings = config["questdb"]  # Extract QuestDB settings from the loaded configuration
 
 class Database:
+    """
+    A class to interact with a database.
+
+    ...
+
+    Attributes
+    ----------
+    None
+
+    Methods
+    -------
+    get_scalar(query:str) -> float:
+        Execute a query and return a scalar.
+    """
+
     def __init__(self):
+        """
+        Constructs all the necessary attributes for the Database object.
+        """
         pass
 
-    def get_scalar(self, query):
+    def get_scalar(self, query: str) -> float:
         """
-        Gets a scalar from a query
+        Execute a SQL query and return a scalar.
 
         Args:
-            query (str): Query to run
+            query (str): SQL query to execute.
 
         Returns:
-            scalar (float): Scalar with the data
+            scalar (float): Scalar result of the query.
         """
-        # prepare url with db config and query
+        # Prepare the URL for the HTTP request by concatenating the host, port, and query parameters
         url = f"http://{questdb_settings['host']}:{questdb_settings['port_web']}/exec?query={query}"
 
-        # get scalar
+        # Send an HTTP GET request to the specified URL
         r = requests.get(url)
 
-        # read scalar
+        # Parse the response text as JSON and extract the scalar result
         return json.loads(r.text)["dataset"][0][0]
